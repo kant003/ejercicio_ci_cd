9d49cb3155ee931228894517add0a0b435e510d6
kant003 <gonzalezm.angel@gmail.com>
2025-12-05 09:11:57 +0100

    add to_upper

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 def elevar(base, exp):
     return base**exp
 
+
+def to_upper(text):
+    return text.upper()
--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,7 @@
-from src.main import elevar
+from src.main import elevar, to_upper
 
 def test_elevar_basico():
     assert elevar(2,3) == 8
+    
+def test_to_upper():
+    assert to_upper('Angel') == 'ANGEL'
